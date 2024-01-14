aa818e29ae7c118cfee3af23fcef9c68449ff863
Anik <anik2002bro@gmail.com>
2024-01-15 04:01:32 +0600

    instructor role has been added to the authorization middleware in the user route.

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -2,31 +2,32 @@ const { userControllers } = require("../api/user");
 const { authMiddleware } = require("../middlewares");
 
 const userRoute = (router) => {
-  // Find All Users
+  // Only admins can find all users.
   router.get(
     "/api/v1/users",
     authMiddleware.authenticate,
     authMiddleware.authorize({ roles: ["admin"] }),
     userControllers.findAll
   );
-  // Find Single,Update and Remove User
+
+  // Admin, authenticate, and authorize users (who are the actual owners) can find a single user, update a user, and delete a user.
   router
     .route("/api/v1/users/:id")
     .get(
       authMiddleware.authenticate,
-      authMiddleware.authorize({ roles: ["admin", "user"] }),
+      authMiddleware.authorize({ roles: ["admin", "instructor", "user"] }),
       authMiddleware.userOwnership({ model: "User" }),
       userControllers.findSingle
     )
     .patch(
       authMiddleware.authenticate,
-      authMiddleware.authorize({ roles: ["admin", "user"] }),
+      authMiddleware.authorize({ roles: ["admin", "instructor", "user"] }),
       authMiddleware.userOwnership({ model: "User" }),
       userControllers.update
     )
     .delete(
       authMiddleware.authenticate,
-      authMiddleware.authorize({ roles: ["admin", "user"] }),
+      authMiddleware.authorize({ roles: ["admin", "instructor", "user"] }),
       authMiddleware.userOwnership({ model: "User" }),
       userControllers.remove
     );
