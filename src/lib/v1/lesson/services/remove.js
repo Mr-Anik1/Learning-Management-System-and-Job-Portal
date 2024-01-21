a4d547ddbecf65b3800c6660f28e32efe8075a74
Anik <anik2002bro@gmail.com>
2024-01-21 10:07:07 +0600

    Some functionality has been added which check lesson is under of the  exact course in the remove course services.

--- src/lib/v1/lesson/services/remove.js
+++ src/lib/v1/lesson/services/remove.js
@@ -20,12 +20,19 @@ const remove = async ({ courseId, lessonId }) => {
     // Find lesson
     const isExistLesson = await singleLessonById({ lessonId });
 
-    // If lesson doesn't exist
+    // If lesson doesn't exist.
     if (!isExistLesson) {
       throw new errors.NotFoundError(`Your Requested Lesson Doesn't Exist`);
     }
 
-    // If lesson is exist, then delete the lesson form the course
+    // Now check does the courseId has exist to this isExistLesson. If the courseId doesn't exist this isExistLesson then throw a error.
+    if (isExistLesson.courseId.toString() !== courseId) {
+      throw new errors.BadRequestError(
+        `Wrong input! This lesson doesn't under of this course.`
+      );
+    }
+
+    // If lesson is exist and the lesson is under of the course, then delete the lesson form the course.
     const deleteLessonFromCourse = await courseServicesV1.update({
       courseId,
       pullLessonId: lessonId,
@@ -49,6 +56,11 @@ const remove = async ({ courseId, lessonId }) => {
     // If error is instance of NotFoundError
     if (err instanceof errors.NotFoundError) {
       throw new errors.NotFoundError(err.message);
+    }
+
+    // If error is instance of BadRequestError
+    if (err instanceof errors.BadRequestError) {
+      throw new errors.BadRequestError(err.message);
     }
 
     throw new errors.InternalServerError("Lesson Deletion Failed");
